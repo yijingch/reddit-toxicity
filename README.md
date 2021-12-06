32ec402bdf85fe94f67ef56e73ff96e66c406313
yijingch <yijingch@umich.edu>
2021-12-06 15:23:47 +0100

    add descriptive nb

--- README.md
+++ README.md
@@ -6,19 +6,18 @@ Class project repository:
 - DNDS 6291: Statistical Methods in Network Science
 
 ## Research questions
-- User-level: whether/how the toxicity level of individual users differ from their neighbors, and whether/how the pattern persists over time
-- Thread-level: whether/how the toxicity level of conversations differ across threads with varying structures (--> an indication of engagement)
+
 
 ### code guide
-- `pipeline_reddit_fixstart_by_year.py`: to crawl Reddit submissions and comments through Pushshift API
+- `pipeline_reddit_fixstart_by_year.py`: crawl Reddit submissions and comments through Pushshift API
 - `parse_to_db.py`: parse .json files to sqlite .db
-- `community-graph-visualization.ipynb`
--
+- `basic-descriptives.ipynb`: produce basic descriptives of the entire dataset
+- ``
 
 ### data and index
 
 - userlist:
-  - `indx/ACTIVEUSERS_ALLYEARS2_10.csv`: users who have posted more than or equal to 10 times in at least 2 subreddits from 2012 to 2015.
-  - `indx/ACTIVEUSERS_ALLYEARS2_20.csv`: users who have posted more than or equal to 20 times in at least 2 subreddits from 2012 to 2015.
-  - `indx/ACTIVEUSERS_ALLYEARS2_50.csv`: users who have posted more than or equal to 50 times in at least 2 subreddits from 2012 to 2015.
-  - `indx/ACTIVEUSERS_ALLYEARS2_50.csv`: users who have posted more than or equal to 100 times in at least 5 subreddits from 2012 to 2015 (a very small subset for pipeline debugging)
+  - `indx/ACTIVEUSERS_ALLYEARS2_10.csv`: users who have posted more than or equal to 10 times in at least 2 submissions from 2012 to 2015.
+  - `indx/ACTIVEUSERS_ALLYEARS2_20.csv`: users who have posted more than or equal to 20 times in at least 2 submissions from 2012 to 2015.
+  - `indx/ACTIVEUSERS_ALLYEARS2_50.csv`: users who have posted more than or equal to 50 times in at least 2 submissions from 2012 to 2015.
+  - `indx/ACTIVEUSERS_ALLYEARS2_50.csv`: users who have posted more than or equal to 100 times in at least 5 submissions from 2012 to 2015 (a very small subset for pipeline debugging)
--- src/utils.py
+++ src/utils.py
@@ -12,65 +12,72 @@ conn1 = sqlite3.connect(DBPATH1)
 # cur1 = conn.cursor()
 
 
-def build_subm_df(year, with_text=True, with_time=False):
+def build_subm_df(year, with_text=True, with_time=False, filter_null=True):
     print("- fetching submissions for year", year)
     if with_text:
         if with_time:
-            qr = f"SELECT id AS submission_id, author, title, selftext, subreddit, toxicity, created_utc FROM 'submission-{year}' "
-            qr += f"WHERE author != '[deleted]' "
-            qr += f"AND (title != '' OR selftext != '')"
+            qr = f"SELECT id AS submission_id, author, title, selftext, subreddit, toxicity, created_utc FROM 'submission-{year}'"
+            if filter_null:
+                qr += f" WHERE author != '[deleted]' "
+                qr += f" AND (title != '' OR selftext != '')"
             df = pd.read_sql_query(qr, conn1)
             df["body"] = df["title"] + " " + df["selftext"]
             df = df.drop(columns=["title", "selftext"])
         else:
-            qr = f"SELECT id AS submission_id, author, title, selftext, subreddit, toxicity FROM 'submission-{year}' "
-            qr += f"WHERE author != '[deleted]' "
-            qr += f"AND (title != '' OR selftext != '')"
+            qr = f"SELECT id AS submission_id, author, title, selftext, subreddit, toxicity FROM 'submission-{year}'"
+            if filter_null:
+                qr += f" WHERE author != '[deleted]' "
+                qr += f" AND (title != '' OR selftext != '')"
             df = pd.read_sql_query(qr, conn1)
             df["body"] = df["title"] + " " + df["selftext"]
             df = df.drop(columns=["title", "selftext"])
     else:
         if with_time:
-            qr = f"SELECT id AS submission_id, author, subreddit, toxicity, created_utc FROM 'submission-{year}' "
-            qr += f"WHERE author != '[deleted]'"
+            qr = f"SELECT id AS submission_id, author, subreddit, toxicity, created_utc FROM 'submission-{year}'"
+            if filter_null:
+                qr += f" WHERE author != '[deleted]'"
             df = pd.read_sql_query(qr, conn1)
         else:
-            qr = f"SELECT id AS submission_id, author, subreddit, toxicity FROM 'submission-{year}' "
-            qr += f"WHERE author != '[deleted]'"
+            qr = f"SELECT id AS submission_id, author, subreddit, toxicity FROM 'submission-{year}'"
+            if filter_null:
+                qr += f" WHERE author != '[deleted]'"
             df = pd.read_sql_query(qr, conn1)
     return df
 
 
-def build_comm_df(year, with_text=True, with_time=False):
+def build_comm_df(year, with_text=True, with_time=False, filter_null=True):
     print("- fetching comments for year", year)
     if with_text:
         if with_time:
-            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, body, subreddit, toxicity, created_utc FROM 'comment-{year}' "
+            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, body, subreddit, toxicity, created_utc FROM 'comment-{year}'"
         else:
-            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, body, subreddit, toxicity FROM 'comment-{year}' "
+            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, body, subreddit, toxicity FROM 'comment-{year}'"
     else:
         if with_time:
-            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, subreddit, toxicity, created_utc FROM 'comment-{year}' "
+            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, subreddit, toxicity, created_utc FROM 'comment-{year}'"
         else:
-            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, subreddit, toxicity FROM 'comment-{year}' "
-    qr += f"WHERE author != '[deleted]' "
-    qr += f"AND body != ''"
+            qr = f"SELECT id AS comment_id, link_id AS submission_id, parent_id, author, subreddit, toxicity FROM 'comment-{year}'"
+    if filter_null:
+        qr += f" WHERE author != '[deleted]' "
+        qr += f" AND body != ''"
     df = pd.read_sql_query(qr, conn1)
     df["submission_id"] = df["submission_id"].map(lambda x: x[3:])
+    # df["parent_id"] = df["parent_id"].map(lambda x: x[3:])
     return df
 
 
-def build_df_year(year, with_text, with_time):
+def build_df_year(year, with_text, with_time, filter_null):
     global USERS, SUBREDDITS
     print(f"building base dataframe for year {year}...")
-    subm_df = build_subm_df(year, with_text=with_text, with_time=with_time)
-    comm_df = build_comm_df(year, with_text=with_text, with_time=with_time)
+    subm_df = build_subm_df(year, with_text=with_text, with_time=with_time, filter_null=filter_null)
+    comm_df = build_comm_df(year, with_text=with_text, with_time=with_time, filter_null=filter_null)
     df_year = pd.concat([subm_df,comm_df])
     df_year["toxicity"] = df_year["toxicity"].fillna(value=np.nan)
     df_year["toxicity"] = df_year["toxicity"].replace(r"^\s*$", np.nan, regex=True)
     df_year["toxicity"] = df_year["toxicity"].astype(float)
     USERS = df_year.author.unique()
     SUBREDDITS = df_year.subreddit.unique()
+    print("- number of entries:", len(df))
     return df_year
 
 
